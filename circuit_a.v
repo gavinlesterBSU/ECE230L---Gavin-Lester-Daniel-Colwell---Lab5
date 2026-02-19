83cfbca427e96d182d06d0920ca6e2d2cdc75f75
danielcolwellbsu <danielcolwell@u.boisestate.edu>
2026-02-19 13:46:31 -0700

    Create circuit_a.v

---
+++ circuit_a.v
@@ -0,0 +1,14 @@
+module circuit_a(
+    input A,
+    input B,
+    input C,
+    input D,
+    output Y
+
+    // Declare inputs
+    // Declare Y output
+);
+    assign Y = (~A & D);
+    // Enter logic equation here
+
+endmodule
